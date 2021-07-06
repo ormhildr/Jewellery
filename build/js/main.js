26f887773df215814dec9494e1f4a6be8d02f595
unknown <bigvolkovski@gmail.com>
2021-07-06 12:11:03 +0300

    Стилизация хэдера для планшетов и мобил

--- build/js/main.js
+++ build/js/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const body = document.body;
+const navMain = document.querySelector(`.page-header__inner`);
+const navToggle = navMain.querySelector(`.page-header__toggle`);
+
+if (navMain !== null) {
+  navMain.classList.remove(`page-header__inner--nojs`);
+  navMain.classList.remove(`page-header__inner--closed-nojs`);
+  navMain.classList.add(`page-header__inner--js`);
+
+  navToggle.addEventListener(`click`, function () {
+    navMain.classList.toggle(`page-header__inner--closed`);
+    navMain.classList.toggle(`page-header__inner--opened`);
+    if (navMain.classList.contains(`page-header__inner--opened`)) {
+      body.style.overflow = `hidden`;
+    }
+    if (navMain.classList.contains(`page-header__inner--closed`)) {
+      body.style.overflow = `visible`;
+    }
+  });
+}
